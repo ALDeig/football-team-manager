3502808221f2794ae0a3ad7e79825881490d9d2c
Denis <alkin.denis@gmail.com>
2023-07-03 17:55:40 +0400

    feat(db): models database and requests

--- app/src/services/db/db_requests.py
+++ app/src/services/db/db_requests.py
@@ -1,11 +1,22 @@
+from collections.abc import Sequence
 import sqlalchemy as sa
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.src.services.db.tables import User
+from app.src.services.db.tables import Player 
 
 
-async def add_user(session: AsyncSession, user_id: int, name: str, role: str):
-    user = User(id=user_id, name=name, role=role)
-    session.add(user)
+async def add_player(session: AsyncSession, name: str, level: float):
+    player = Player(name=name, level=level)
+    session.add(player)
     await session.commit()
+
+
+async def delete_player(session: AsyncSession, name: str):
+    await session.execute(sa.delete(Player).where(Player.name == name))
+    await session.commit()
+
+
+async def get_players(session: AsyncSession) -> Sequence[Player]:
+    players = await session.scalars(sa.select(Player))
+    return players.all()
 
