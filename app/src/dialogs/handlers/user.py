82019126102e916f650f94846e4bc7bbe3c18a21
Denis <alkin.denis@gmail.com>
2023-10-13 15:06:09 +0400

    feat: добавил возможность создать больше 2 команд

--- app/src/dialogs/handlers/user.py
+++ app/src/dialogs/handlers/user.py
@@ -1,3 +1,4 @@
+import enum
 from aiogram import F, Bot, Router
 from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
@@ -5,7 +6,7 @@ from aiogram.types import CallbackQuery, Message
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.src.dialogs.keyboards.kb_create_team import kb_select_players
-from app.src.dialogs.keyboards.user import kb_after_created_teams, kb_again_shuffle_teams
+from app.src.dialogs.keyboards.user import kb_after_created_teams
 from app.src.services.db import db_requests
 from app.src.services.teams import create_team_text, create_teams
 
@@ -19,59 +20,87 @@ async def cmd_start(msg: Message, state: FSMContext):
     await msg.answer("Работаю...")
 
 
-@router.message(Command(commands="create_teams"), flags={"db": True})
-async def cmd_create_teams(msg: Message, db: AsyncSession, state: FSMContext):
+@router.message(Command(commands="create_teams"))
+async def cmd_create_teams(msg: Message, state: FSMContext):
+    await msg.answer("Введи количество команд")
+    await state.set_state("get_number_of_teams")
+
+
+@router.message(StateFilter("get_number_of_teams"), flags={"db": True})
+async def get_number_of_teams(msg: Message, db: AsyncSession, state: FSMContext):
+    if msg.text is None or not msg.text.isdigit():
+        await msg.answer("Количество должно быть числом")
+        return
     players = await db_requests.get_players(db)
-    await state.update_data(players=players, selected_players=[])
     kb = kb_select_players(players, [])
     await msg.answer("Выбери игроков", reply_markup=kb)
+    msg_amount_players = await msg.answer("Количество выбранных игроков - 0")
+    await state.update_data(
+        players=players,
+        selected_players=[],
+        num_of_teams=int(msg.text),
+        msg_amount_players=msg_amount_players.message_id,
+    )
     await state.set_state("select_players")
 
 
+# @router.message(Command(commands="create_teams"), flags={"db": True})
+# async def cmd_create_teams(msg: Message, db: AsyncSession, state: FSMContext):
+#     players = await db_requests.get_players(db)
+#     await state.update_data(players=players, selected_players=[])
+#     kb = kb_select_players(players, [])
+#     await msg.answer("Выбери игроков", reply_markup=kb)
+#     await state.set_state("select_players")
+
+
 @router.callback_query(
-    StateFilter("select_players"),
-    F.data.in_(("finish", "again_shuffle"))
+    StateFilter("select_players"), F.data.in_(("finish", "again_shuffle"))
 )
 async def btn_finish_players_select(call: CallbackQuery, state: FSMContext):
-    if call.message is None: return
+    if call.message is None:
+        return
     data = await state.get_data()
-    team_1, team_2 = create_teams(data["players"], data["selected_players"])
-    await state.update_data(team_1=team_1, team_2=team_2)
-    text_team_1 = create_team_text(1, team_1, "hide")
-    text_team_2 = create_team_text(2, team_2, "hide")
-    msg_team_1 = await call.message.answer(text_team_1)
-    msg_team_2 = await call.message.answer(
-        text_team_2,
-        reply_markup=kb_after_created_teams(call.from_user.id, "hide")
+    teams = create_teams(
+        data["num_of_teams"], data["players"], data["selected_players"]
     )
-    await state.update_data(
-        team_1=team_1,
-        team_2=team_2,
-        msg_id_team_1=msg_team_1.message_id,
-        msg_id_team_2=msg_team_2.message_id
+    texts = [
+        create_team_text(number, team, "hide") for number, team in enumerate(teams, 1)
+    ]
+    msgs_teams = []
+    for text in texts[:-1]:
+        msg = await call.message.answer(text)
+        msgs_teams.append(msg.message_id)
+    msg_last_team = await call.message.answer(
+        texts[-1], reply_markup=kb_after_created_teams(call.from_user.id, "hide")
     )
-    # await state.clear()
+    msgs_teams.append(msg_last_team.message_id)
+    await state.update_data(teams=teams, msgs_teams=msgs_teams)
 
 
 @router.callback_query(StateFilter("select_players"), F.data.in_(("hide", "show")))
 async def btn_show_or_hide_team_level(call: CallbackQuery, bot: Bot, state: FSMContext):
-    if call.data is None or call.message is None: return
+    if call.data is None or call.message is None:
+        return
     await call.answer()
     data = await state.get_data()
-    text_team_1 = create_team_text(1, data["team_1"], call.data)
-    text_team_2 = create_team_text(2, data["team_2"], call.data)
-    await bot.edit_message_text(text_team_1, call.from_user.id, data["msg_id_team_1"])
+    texts = [
+        create_team_text(number, team, call.data)
+        for number, team in enumerate(data["teams"], 1)
+    ]
+    for index, text in enumerate(texts[:-1]):
+        await bot.edit_message_text(text, call.from_user.id, data["msgs_teams"][index])
     await bot.edit_message_text(
-        text=text_team_2, 
+        text=texts[-1],
         chat_id=call.from_user.id,
-        message_id=data["msg_id_team_2"],
-        reply_markup=kb_after_created_teams(call.from_user.id, call.data)
+        message_id=data["msgs_teams"][-1],
+        reply_markup=kb_after_created_teams(call.from_user.id, call.data),
     )
 
 
 @router.callback_query(StateFilter("select_players"))
-async def btn_select_player(call: CallbackQuery, state: FSMContext):
-    if call.data is None or call.message is None: return
+async def btn_select_player(call: CallbackQuery, bot: Bot, state: FSMContext):
+    if call.data is None or call.message is None:
+        return
     action, player_id = call.data.split(":")
     data = await state.get_data()
     selected_players: list[str] = data["selected_players"]
@@ -81,4 +110,8 @@ async def btn_select_player(call: CallbackQuery, state: FSMContext):
         selected_players.pop(selected_players.index(player_id))
     kb = kb_select_players(data["players"], selected_players)
     await call.message.edit_reply_markup(reply_markup=kb)
-
+    await bot.edit_message_text(
+        f"Количество выбранных игроков - {len(selected_players)}",
+        call.from_user.id,
+        data["msg_amount_players"],
+    )
--- app/src/services/teams.py
+++ app/src/services/teams.py
@@ -1,52 +1,25 @@
 from collections import defaultdict
 from random import randint, shuffle
 
-from aiogram.types import InlineKeyboardMarkup
-from app.src.dialogs.keyboards.user import kb_after_created_teams
-
 from app.src.services.db.tables import Player
 
 
-def get_response_to_creatation_team_request(
-        user_id: int,
-        players: list[Player],
-        selected_players: list[str]) -> tuple[str, str, InlineKeyboardMarkup]:
-    team_1, team_2 = create_teams(players, selected_players)
-    text_team_1 = create_team_text(1, team_1, "hide")
-    text_team_2 = create_team_text(2, team_2, "hide")
-    kb = kb_after_created_teams(user_id, "hide")
-    return text_team_1, text_team_2, kb
-
-
 def create_teams(
-        players: list[Player],
-        selected_players: list[str]) -> tuple[dict[str, float], dict[str, float]]:
+    num_of_teams: int, players: list[Player], selected_players_id: list[str]
+) -> list[dict[str, float]]:
     players_dict = {}
-    for player_id in selected_players:
-        player, = (a for a in players if a.id == player_id)
+    for player_id in selected_players_id:
+        (player,) = (a for a in players if a.id == player_id)
         players_dict[player.name] = player.level
-
-    team_1, team_2 = _form_teams(players_dict)
-    return team_1, team_2
-
-
-def create_team_text(team_number: int, team: dict[str, float], action: str) -> str:
-    text = f"Команда {team_number}\nКоличество игроков: {len(team)}\n\n"
-    if action == "show":
-        text += f"Уровень - {sum(level for level in team.values())}\n\n"
-    for player_name, level in team.items():
-        player_text = f"<b>{player_name}</b>" 
-        if action == "show":
-            player_text += f" - {level}\n"
-        else:
-            player_text += "\n"
-        text += player_text
-    return text
+    players_by_levels = defaultdict(list)
+    for name, level in players_dict.items():
+        players_by_levels[level].append(name)
+    teams = _form_teams(num_of_teams, players_dict)
+    return teams
 
 
-def _form_teams(players: dict[str, float]) -> tuple[dict[str, float], dict[str, float]]:
-    team_1 = []
-    team_2 = []
+def _form_teams(num_of_teams: int, players: dict[str, float]) -> list[dict[str, float]]:
+    teams = [[] for _ in range(num_of_teams)]
     players_by_levels = defaultdict(list)
     for name, level in players.items():
         players_by_levels[level].append(name)
@@ -54,23 +27,35 @@ def _form_teams(players: dict[str, float]) -> tuple[dict[str, float], dict[str, 
     for level in levels:
         players_by_level = players_by_levels[level]
         for _ in range(len(players_by_level)):
-            selected_player = players_by_level.pop(randint(0, len(players_by_level) - 1))
-            _append_player_in_team(team_1, team_2, players, selected_player)
-    shuffle(team_1)
-    shuffle(team_2)
-    team_1 = {name: players[name] for name in team_1}
-    team_2 = {name: players[name] for name in team_2}
-    return team_1, team_2
+            selected_player = players_by_level.pop(
+                randint(0, len(players_by_level) - 1)
+            )
+            _append_player_in_team(teams, players, selected_player)
+    for team in teams:
+        shuffle(team)
+    teams_with_level = [{name: players[name] for name in team} for team in teams]
+    return teams_with_level
 
 
 def _append_player_in_team(
-        team_1: list[str],
-        team_2: list[str],
-        players: dict[str, float],
-        player: str) -> None:
-    team_1_level = sum(players[player] for player in team_1)
-    team_2_level = sum(players[player] for player in team_2)
-    if team_1_level < team_2_level:
-        team_1.append(player)
-    else:
-        team_2.append(player)
+    teams: list[list[str]], players: dict[str, float], player: str
+) -> None:
+    teams_level = []
+    for team in teams:
+        teams_level.append(sum(players[player] for player in team))
+    min_level = min(teams_level)
+    teams[teams_level.index(min_level)].append(player)
+
+
+def create_team_text(team_number: int, team: dict[str, float], action: str) -> str:
+    text = f"Команда {team_number}\nКоличество игроков: {len(team)}\n\n"
+    if action == "show":
+        text += f"Уровень - {sum(level for level in team.values())}\n\n"
+    for player_name, level in team.items():
+        player_text = f"<b>{player_name}</b>"
+        if action == "show":
+            player_text += f" - {level}\n"
+        else:
+            player_text += "\n"
+        text += player_text
+    return text
